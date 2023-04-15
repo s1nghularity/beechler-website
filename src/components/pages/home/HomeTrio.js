89705fffe4b23c68ad643401f5f649eeb4257aa4
Vikram Singh <v.p.singh@gmail.com>
2023-04-14 22:15:34 -0700

    Beechler highlights first draft, attempting to create hexagons with rounded corners.

--- src/components/pages/home/HomeTrio.js
+++ src/components/pages/home/HomeTrio.js
@@ -1,46 +1,62 @@
 import React from "react";
-import { Container, Row, Carousel } from "react-bootstrap";
-import { faStar, faCrown, faHistory, faGlobe } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container } from "react-bootstrap";
+import StarIcon from "@mui/icons-material/Star";
+import CrownIcon from "@mui/icons-material/MilitaryTech";
+import HistoryIcon from "@mui/icons-material/History";
+import GlobeIcon from "@mui/icons-material/Public";
 import "../../../styles/HomePage.css";
 import "../../../styles/theme.css"; // Import your theme.css
 
+const GradientIcon = ({ icon: Icon }) => {
+  return (
+    <>
+      <svg width="0" height="0">
+        <linearGradient
+          id="accent-gradient"
+          x1="100%"
+          y1="100%"
+          x2="0%"
+          y2="0%"
+        >
+          <stop stopColor="#fbb034" offset="0%" />
+          <stop stopColor="#ffdd00" offset="74%" />
+        </linearGradient>
+      </svg>
+      <Icon sx={{ fill: "url(#accent-gradient)" }} />
+    </>
+  );
+};
+
+const Hexagon = ({ icon: Icon, text }) => {
+  return (
+    <div className="hilightCard">
+      <div className="hexagon-container">
+        <div className="hexagon">
+          <div className="hexagon-inner">
+            <GradientIcon icon={Icon} className="hilightIcon" />
+          </div>
+        </div>
+      </div>
+      <p className="hilight-text">{text}</p>
+    </div>
+  );
+};
+
 function HomeTrio() {
   const traits = [
-    { icon: faStar, text: "Musicians Choice" },
-    { icon: faCrown, text: "Unrivaled Handmade Quality" },
-    { icon: faHistory, text: "Industry Veteran" },
-    { icon: faGlobe, text: "Internationally Adored" },
+    { icon: StarIcon, text: "Musicians Choice" },
+    { icon: CrownIcon, text: "Unrivaled Handmade Quality" },
+    { icon: HistoryIcon, text: "Industry Veteran" },
+    { icon: GlobeIcon, text: "Internationally Adored" },
   ];
 
   return (
-    <Container className="my-5 homeTrioContainer">
-      <Row className="justify-content-center">
+    <Container fluid className="hilightContainer">
+      <div className="d-flex justify-content-center">
         {traits.map((trait, index) => (
-          <div key={index} className="trioCard">
-            <div className="hexagon">
-              <svg className="icon-svg" viewBox="0 0 32 32" style={{ width: "2.5rem", height: "2.5rem" }}>
-                <FontAwesomeIcon icon={trait.icon} className="homeTrioIcon icon-gradient" />
-              </svg>
-            </div>
-            <p className="homeTrioText">{trait.text}</p>
-          </div>
+          <Hexagon key={index} icon={trait.icon} text={trait.text} />
         ))}
-      </Row>
-      <Carousel className="d-md-none">
-        {traits.map((trait, index) => (
-          <Carousel.Item key={index}>
-            <div className="trioCard d-flex justify-content-center">
-              <div className="hexagon">
-                <svg className="icon-svg" viewBox="0 0 32 32" style={{ width: "2.5rem", height: "2.5rem" }}>
-                  <FontAwesomeIcon icon={trait.icon} className="homeTrioIcon icon-gradient" />
-                </svg>
-              </div>
-              <p className="homeTrioText">{trait.text}</p>
-            </div>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      </div>
     </Container>
   );
 }
