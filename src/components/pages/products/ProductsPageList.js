7e8ccc11b51130a3ad73dba5f42fb9c79ae80d3e
Vikram Singh <v.p.singh@gmail.com>
2023-04-25 19:07:13 -0700

    Updates to products page.
    -Changing orientation of card so image is on top product description below.
    -Continued refinement of product description section.

--- src/components/pages/products/ProductsPageList.js
+++ src/components/pages/products/ProductsPageList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card, Row, Col } from "react-bootstrap";
-import "../../../styles/ProductsPage.css";
 import { Chip, Avatar } from "@mui/material";
-import BoreCircle from "@mui/icons-material/Category";
+import { styled } from "@mui/system";
 import FacingIcon from "@mui/icons-material/TextFields";
 import saxIcon from "../../../assets/img/products/saxophone.png";
 import clarinetIcon from "../../../assets/img/products/clarinet.png";
+import "../../../styles/ProductsPage.css";
 
 const getChipColor = (subtype) => {
   switch (subtype) {
@@ -23,6 +23,79 @@ const getChipColor = (subtype) => {
       return "";
   }
 };
+
+const StyledChip = styled(Chip)(({ subtype }) => {
+  const chipColor = getChipColor(subtype);
+  return {
+    backgroundColor: chipColor,
+    "& .MuiAvatar-root": {
+      backgroundColor: chipColor,
+    },
+  };
+});
+
+const CustomChip = ({ subtype, ...rest }) => (
+  <StyledChip subtype={subtype} avatar={<Avatar />} {...rest} />
+);
+
+const ProductImage = ({ image }) => (
+  <div className="product-image">
+    <img src={image} alt="Product" />
+  </div>
+);
+
+
+const ProductDescription = ({ product, id }) => (
+  <div className="product-details">
+    <Card.Body className="d-flex flex-column h-100">
+
+      <div className="header-section">
+        
+        <img
+          className="instrument-icon"
+          src={product.instrument === "Saxophone" ? saxIcon : clarinetIcon}
+          alt={product.instrument}
+        />
+    <p className="product-id">{product.id}</p>
+
+        {product.subtype && (
+          <CustomChip
+            variant="outlined"
+            size="small"
+            label={product.subtype}
+            avatar={<Avatar>{product.subtype[0]}</Avatar>}
+            style={{
+              borderColor: getChipColor(product.subtype),
+              color: getChipColor(product.subtype),
+            }}
+          />
+        )}
+      </div>
+      {product.bore && <p className="bore-specification">Bore: {product.bore}</p>}
+
+      <Card.Text className="price mt-auto">Price: {product.price}</Card.Text>
+      <Card.Text>Tip Opening: 1-10</Card.Text>
+      <Card.Text className="facing">
+        Facing:{" "}
+        {product.facing.map((facing, index) => (
+          <FacingIcon key={index} titleAccess={facing} />
+        ))}
+      </Card.Text>
+    </Card.Body>
+  </div>
+);
+
+const ProductCard = ({ product }) => (
+  <Col md={4} className="mb-4" key={product.id}>
+    <Card className="h-100 border-0 shadow-sm product-card">
+      <div className="d-flex flex-column product-card-container">
+        <ProductImage image={product.image} id={product.id} />
+        <ProductDescription product={product} />
+      </div>
+    </Card>
+  </Col>
+);
+
 
 const ProductsPageList = ({ products }) => {
   const productsByCategory = products.reduce((acc, product) => {
@@ -42,71 +115,7 @@ const ProductsPageList = ({ products }) => {
               <h2 className="category-title">{category}</h2>
             </Col>
             {categoryProducts.map((product) => (
-              <Col md={4} className="mb-4" key={product.id}>
-                <Card className="h-100 border-0 shadow-sm rounded product-card">
-                  <div className="d-flex" style={{ height: "100%" }}>
-                    <div className="product-image">
-                      <img
-                        src={product.image}
-                        alt={product.category}
-                        style={{ height: "250px", objectFit: "contain" }}
-                      />
-                      <span className="product-id">{product.id}</span>
-                    </div>
-                    <div className="product-details" style={{ flex: 1 }}>
-                      <Card.Body className="d-flex flex-column h-100">
-                        <div
-                          style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                          }}
-                        >
-                          <span
-                            className="instrument-icon"
-                            style={{
-                              backgroundImage: `url(\${product.instrument === 'Saxophone' ? saxIcon : clarinetIcon})`,
-                              backgroundSize: "contain",
-                              width: "24px",
-                              height: "24px",
-                            }}
-                          />
-                          {product.subtype && (
-                            <Chip
-                              variant="outlined"
-                              size="small"
-                              label={product.subtype}
-                              avatar={<Avatar>{product.subtype[0]}</Avatar>}
-                              style={{
-                                borderColor: getChipColor(product.subtype),
-                                color: getChipColor(product.subtype),
-                              }}
-                            />
-                          )}
-                        </div>
-                        {product.bore && (
-                          <p className="bore-specification">Bore: {product.bore}</p>
-
-                        )}
-
-                        <Card.Text
-                          className="mt-auto"
-                          style={{ textAlign: "right" }}
-                        >
-                          Price: {product.price}
-                        </Card.Text>
-                        <Card.Text>Tip Opening: 1-10</Card.Text>
-                        <Card.Text>
-                          Facing:{" "}
-                          {product.facing.map((facing, index) => (
-                            <FacingIcon key={index} titleAccess={facing} />
-                          ))}
-                        </Card.Text>
-                      </Card.Body>
-                    </div>
-                  </div>
-                </Card>
-              </Col>
+              <ProductCard product={product} />
             ))}
           </>
         )
@@ -116,3 +125,5 @@ const ProductsPageList = ({ products }) => {
 };
 
 export default ProductsPageList;
+
+ 
--- src/components/pages/products/ProductsPageNav.js
+++ src/components/pages/products/ProductsPageNav.js
@@ -1,77 +1,88 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, NavItem } from 'react-bootstrap';
 import '../../../styles/ProductsPage.css';
+
+
+
+const saxIcon = require('../../../assets/img/products/saxophone.png').default;
+const clarinetIcon = require('../../../assets/img/products/clarinet.png').default;
+
+
+
+
+
 
 const ProductsPageNav = ({ handleCategorySelect, handleInstrumentSelect, handleSubtypeSelect}) => {
   return (
     <Navbar className='products-nav' bg="light" expand="lg">
-      <Navbar.Brand href="#">Categories</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarToggler" />
       <Navbar.Collapse id="navbarToggler">
         <Nav className="mr-auto">
 
-          <NavDropdown title="Categories" id="navbarDropdown">        
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler White Diamond')}>
-            Beechler White Diamond
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Tonalex')}>
-            Beechler Tonalex
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Legacy')}>
-            Beechler Legacy
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Custom')}>
-            Beechler Custom
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Bellite')}>
-            Beechler Bellite
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Bellite Custom')}>
-            Beechler Bellite Custom
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('ARB Metal')}>
-            ARB Metal
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('ARB Custom Metal')}>
-            ARB Custom Metal
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler & ARB Ligatures')}>
-            Beechler & ARB Ligatures
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler & ARB Caps')}>
-            Beechler & ARB Caps
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Pegs')}>
-            Beechler Pegs
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleCategorySelect('Accessories')}>
-            Accessories
-            </NavDropdown.Item>            
-          </NavDropdown>
-          
-          <NavDropdown title="Instrument" id="navInstrument">
-            <NavDropdown.Item onClick={() => handleInstrumentSelect('Saxophone')}>
-            <i className="fas fa-saxophone"></i> Saxophone
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleInstrumentSelect('Clarinet')}>
-            <i className="fas fa-clarinet"></i>Clarinet
-            </NavDropdown.Item>
-          </NavDropdown>
+        <NavItem title="Instrument" id="navInstrument" onClick={() => handleInstrumentSelect('Saxophone')}>
+          <img className='instrument-icon' src={saxIcon}></img> 
+        </NavItem>
+        
+        <NavItem title="Instrument" id="navInstrument" onClick={() => handleInstrumentSelect('Clarinet')}>
+          <img className='instrument-icon' src={clarinetIcon}></img>
+        </NavItem>
+        
 
-          <NavDropdown title="Subtype" id="navSubtype">
-            <NavDropdown.Item onClick={() => handleSubtypeSelect('Soprano')}>
-              Soprano
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleSubtypeSelect('Alto')}>
-              Alto
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleSubtypeSelect('Tenor')}>
-              Tenor
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={() => handleSubtypeSelect('Baritone')}>
-              Baritone
-            </NavDropdown.Item>
-          </NavDropdown>
+        <NavDropdown title="Categories" id="navbarDropdown">        
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler White Diamond')}>
+          Beechler White Diamond
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Tonalex')}>
+          Beechler Tonalex
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Legacy')}>
+          Beechler Legacy
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Custom')}>
+          Beechler Custom
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Bellite')}>
+          Beechler Bellite
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Bellite Custom')}>
+          Beechler Bellite Custom
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('ARB Metal')}>
+          ARB Metal
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('ARB Custom Metal')}>
+          ARB Custom Metal
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler & ARB Ligatures')}>
+          Beechler & ARB Ligatures
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler & ARB Caps')}>
+          Beechler & ARB Caps
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Beechler Pegs')}>
+          Beechler Pegs
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleCategorySelect('Accessories')}>
+          Accessories
+          </NavDropdown.Item>            
+        </NavDropdown>
+        
+
+
+        <NavDropdown title="Subtype" id="navSubtype">
+          <NavDropdown.Item onClick={() => handleSubtypeSelect('Soprano')}>
+            Soprano
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleSubtypeSelect('Alto')}>
+            Alto
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleSubtypeSelect('Tenor')}>
+            Tenor
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleSubtypeSelect('Baritone')}>
+            Baritone
+          </NavDropdown.Item>
+        </NavDropdown>
 
         </Nav>
       </Navbar.Collapse>
