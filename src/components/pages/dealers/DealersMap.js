9b71f162f77ccb5d26374de2f112539691acf1b7
Vikram Singh <v.p.singh@gmail.com>
2023-04-29 08:55:13 -0700

    Continued dealersmap debug

--- src/components/pages/dealers/DealersMap.js
+++ src/components/pages/dealers/DealersMap.js
@@ -1,28 +1,42 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import {locationData} from './DealerData.js';
-import { render } from "react-dom";
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import { locationData } from './DealerData';
+import "../../../styles/DealersPage.css";
 
-const position = [51.505, -0.09]
 
-const DealerMap = () => {
-    return (
-        render(
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
-        )
-    );
+const musicNoteIcon = L.icon({
+  iconUrl: '/path/to/music-note-icon.png',
+  iconSize: [38, 95],
+  iconAnchor: [22, 94],
+});
+
+const DealersMap = () => {
+    console.log('Dealers component rendered'); 
+  return (
+    <MapContainer className="map" center={[51.505, -0.09]} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      />
+      {locationData.map((location) => (
+        <Marker
+          key={location.name}
+          position={[location.latitude, location.longitude]}
+          icon={musicNoteIcon}
+        >
+          <Popup>
+            <h2>{location.name}</h2>
+            <p>{location.address}</p>
+            <p>
+              {location.city}, {location.state} {location.zip}
+            </p>
+            <a href={location.website}>Website</a>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 };
 
-export default DealerMap;
-     
-  
+export default DealersMap;
