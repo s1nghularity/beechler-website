05216d0e4b2a5534b2ecb25e41210f8c696eae22
Vikram Singh <v.p.singh@gmail.com>
2023-05-10 16:30:40 -0700

    Revised Redner debug for Categories section.

--- src/components/pages/home/HomeArtistCard.js
+++ src/components/pages/home/HomeArtistCard.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
-import { animate, stagger } from 'motion';
+import { motion, useAnimation } from 'framer-motion';
 import "../../../styles/theme.css";
 import "../../../styles/HomeArtistCard.css";
-
-
 import alexhan from '../../../assets/img/home/artists/artist-alex-han.png';
 import anthony from '../../../assets/img/home/artists/artist-anthony-terry.png';
 import caryg from '../../../assets/img/home/artists/artist-cary-simms.png';
@@ -11,61 +9,46 @@ import yellowBlob1 from '../../../assets/img/home/artists/y-blob-1.svg';
 import orangeBlob1 from '../../../assets/img/home/artists/o-blob-1.svg';
 import yellowBlob2 from '../../../assets/img/home/artists/y-blob-2.svg';
 
-function HomeArtistCard () {
-    useEffect(() => {
-        const targets = document.querySelectorAll(".artist-card");
-        
-        targets.forEach((target, index) => {
-          animate(target, { y: -100, opacity: 1 }, {
-            delay: index * 0.3, // Add stagger delay based on the index
-            duration: 1.8,
-            y: 0,
-            opacity: 0,
-            easing: "ease-out",
-          });
-        });
-      }, []);
-      
-      
+const artists = [
+  { name: "Alex Han", img: alexhan, blob: yellowBlob1 },
+  { name: "Anthony Terry", img: anthony, blob: orangeBlob1 },
+  { name: "Cary Simms", img: caryg, blob: yellowBlob2 },
+];
+
+const ArtistCard = ({ artist, index }) => {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({ y: -100, opacity: 1 });
+  }, []);
 
   return (
-    <div className="artist-cards-container">
-
-        <div className="artist-card">
-            <div className="image-container">
-                <img className="artist-image" src={alexhan} alt="Alex Han" />
-                <div className="svg-background">
-                <img src={yellowBlob1} alt="Alex Han blob" />
-                <h3>Alex Han</h3>
-                </div>
-            </div>
+    <motion.div
+      className="artist-card"
+      animate={controls}
+      initial={{ y: 0, opacity: 0 }}
+      transition={{ delay: index * 0.3, duration: 1.8, easing: "ease-out" }}
+    >
+      <div className="image-container">
+        <img className="artist-image" src={artist.img} alt={artist.name} />
+        <div className="svg-background">
+          <img src={artist.blob} alt={`${artist.name} blob`} />
+          <h3>{artist.name}</h3>
         </div>
-
-        <div className="artist-card">
-            <div className="image-container">
-                <img className="artist-image" src={anthony} alt="Anthony Terry" />
-                <div className="svg-background">
-                    <img src={orangeBlob1} alt="Anthony Terry blob" />
-                    <h3>Anthony Terry</h3>
-                </div>
-            </div>
-        </div>
-
-        <div className="artist-card">
-            <div className="image-container">
-                <img className="artist-image" src={caryg} alt="Cary Simms" />
-                <div className="svg-background">
-                    <img src={yellowBlob2} alt="Cary Simms blob" />
-                    <h3>Cary Simms</h3>
-                </div>
-            </div>
-        </div>
-        <span>
-        <h2 className="artist-card-header">Featured Artists</h2>
-        </span>
-
-    </div>
+      </div>
+    </motion.div>
   );
 };
+
+function HomeArtistCard() {
+  return (
+    <div className="artist-cards-container">
+      <h2 className="artist-card-header">Featured Artists</h2>
+      {artists.map((artist, index) => (
+        <ArtistCard key={artist.name} artist={artist} index={index} />
+      ))}
+    </div>
+  );
+}
 
 export default HomeArtistCard;
