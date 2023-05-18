66ba1259ef4b5fea6dfcc3a3a33fa8da906add73
Vikram Singh <v.p.singh@gmail.com>
2023-05-17 23:54:50 -0700

    Debugging mobile navbar - refining both JS and CSS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,108 +11,95 @@ import logo from "../assets/img/home/beechler-logo-t.png";
 
 function NavBar() {
   const [expanded, setExpanded] = useState(false);
+  const [togglerToggled, setTogglerToggled] = useState(false);
   const location = useLocation();
-  const currentPath = location.pathname;
+
+  useEffect(() => {
+    setExpanded(false);
+    setTogglerToggled(false);
+  }, [location.pathname]);
+
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 767);
+
+  useEffect(() => {
+    const handleResize = () => setIsDesktop(window.innerWidth > 767);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const collapseStyles = isDesktop
+    ? { maxHeight: "none" }
+    : expanded
+    ? { maxHeight: "500px", transition: "max-height .5s ease-in-out" }
+    : {
+        maxHeight: "0px",
+        transition: "max-height .5s ease-in-out",
+        overflow: "hidden",
+      };
+
+  const handleToggleClick = () => {
+    setExpanded(!expanded);
+    setTogglerToggled(!togglerToggled);
+  };
+
+  const handleContactClick = () => {
+    setExpanded(false);
+    setTogglerToggled(false);
+    window.location.href = "/";
+    ScrollLink.scrollToTop();
+    ScrollLink.scrollTo("#contact");
+  };
+
+  const links = [
+    { path: "/products", label: "Products" },
+    { path: "/tip-openings", label: "Tip Openings" },
+    { path: "/dealers", label: "Dealers" },
+    { path: "/artists", label: "Artists" },
+    { path: "/about", label: "About" },
+  ];
 
   return (
     <Navbar expanded={expanded} expand="md" className="stripe">
-      <div className="navbar-wrapper">
-      <Navbar.Toggle
+
+      <div className="navbar-header">
+      {!isDesktop && (
+        <Navbar.Toggle
           aria-controls="basic-navbar-nav"
-          onClick={(e) => {
-            console.log("onClick", expanded);
-            setExpanded(expanded ? false : "true");
-            const spanElements = e.currentTarget.querySelectorAll(".icon-bar");
-            spanElements.forEach((span) => span.classList.toggle("toggled"));
-          }}
-          className="navbar-toggler"
+          onClick={handleToggleClick}
+          className={`hamburger ${togglerToggled ? "toggled" : ""}`}
         >
-          <span className="icon-bar top-bar"></span>
-          <span className="icon-bar middle-bar"></span>
-          <span className="icon-bar bottom-bar"></span>
-      </Navbar.Toggle>
-
-        <NavbarBrand href="/" className="navbar-brand">
-          <img src={logo} alt="Logo" width="100" height="50" />
-        </NavbarBrand>
-
-
-
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className={`mr-auto super-title ${expanded ? "d-lg-none" : ""}`}>
-            <Nav.Link
-              as={Link}
-              to="/products"
-              className={currentPath === "/products" ? "active-link" : ""}
-            >
-              Products
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/tip-openings"
-              className={currentPath === "/tip-openings" ? "active-link" : ""}
-            >
-              Tip Openings
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/dealers"
-              className={currentPath === "/dealers" ? "active-link" : ""}
-            >
-              Dealers
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/artists"
-              className={currentPath === "/artists" ? "active-link" : ""}
-            >
-              Artists
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/about"
-              className={currentPath === "/about" ? "active-link" : ""}
-            >
-              About
-            </Nav.Link>
-
-            <Nav.Link>
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                onClick={() => {
-                  const scrollToContact = () => {
-                    const contactElement = document.getElementById("contact");
-
-                    if (contactElement) {
-                      window.scrollTo({
-                        top: contactElement.offsetTop,
-                        behavior: "smooth",
-                      });
-                    } else {
-                      console.error("Element with ID 'contact' not found");
-                    }
-                  };
-
-                  if (currentPath === "/") {
-                    scrollToContact();
-                  } else {
-                    window.location.href = "/#contact";
-                    window.addEventListener(
-                      "hashchange",
-                      () => {
-                        scrollToContact();
-                      },
-                      { once: true }
-                    );
-                  }
-                }}
-              />
-            </Nav.Link>
-
-          </Nav>
-        </Navbar.Collapse>
+          <span
+            className={`icon-bar top-bar ${togglerToggled ? "toggled" : ""}`}
+          ></span>
+          <span
+            className={`icon-bar middle-bar ${togglerToggled ? "toggled" : ""}`}
+          ></span>
+          <span
+            className={`icon-bar bottom-bar ${togglerToggled ? "toggled" : ""}`}
+          ></span>
+        </Navbar.Toggle>
+      )}
+      <NavbarBrand href="/" className="navbar-brand">
+        <img src={logo} alt="Logo" width="100" height="50" />
+      </NavbarBrand>
       </div>
 
+      <Nav className={`mr-auto `} style={collapseStyles}>
+        {links.map(({ path, label }) => (
+          <Nav.Link
+            key={path}
+            as={Link}
+            to={path}
+            className={location.pathname === path ? "nav-link active-link" : ""}
+          >
+            {label}
+          </Nav.Link>
+        ))}
+
+        <Nav.Link className= 'contact-button'onClick={handleContactClick}>
+          <FontAwesomeIcon icon={faEnvelope} />
+        </Nav.Link>
+      </Nav>
     </Navbar>
   );
 }
