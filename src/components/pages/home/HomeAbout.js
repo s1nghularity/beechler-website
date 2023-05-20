1990c5e81a98c985bb93139b89b21adad83d1aca
Vikram Singh <v.p.singh@gmail.com>
2023-05-20 10:17:31 -0700

    Further responsive refinement for Our Story and Our Selection sections.

--- src/components/pages/home/HomeAbout.js
+++ src/components/pages/home/HomeAbout.js
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Carousel } from "react-responsive-carousel";
+import { Container, Row, Col } from 'react-bootstrap';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import about1 from "../../../assets/img/home/about/about1.jpg";
 import about2 from "../../../assets/img/home/about/about2.jpg";
@@ -7,12 +9,37 @@ import about4 from "../../../assets/img/home/remle-ad-1949.jpg";
 import "../../../styles/HomeAbout.css";
 
 function HomeAbout() {
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      const screenWidth = window.innerWidth;
+      const customColumnWidth = getCustomColumnWidth(screenWidth);
+      document.documentElement.style.setProperty(
+        "--custom-column-width",
+        customColumnWidth
+      );
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    // Cleanup the event listener
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const getCustomColumnWidth = (screenWidth) => {
+    if (screenWidth >= 768 && screenWidth <= 1312) {
+      return "15%";
+    }
+
+    return null;
+  };
   
   function handleSlideChange(index) {
-    console.log("Slide changed to:", index);
     const slides = document.querySelectorAll(".slide");
     slides.forEach((slide, i) => {
-      const caption = slide.querySelector(".about-section-tag");
+      const caption = slide.querySelector(".our-story-tag");
       if (caption) {
         if (i === index) {
           slide.classList.add("fadeInMoveRight");
@@ -26,9 +53,9 @@ function HomeAbout() {
   }
 
   return (
-    <div className="home-about container-fluid stripe-home-about">
-      <div className="row home-about-row">
-        <div className="col-md-4 home-about-left">
+    <Container fluid className="home-about stripe-home-about">
+      <Row className="home-about-row">
+        <Col md={4} lg={3} className="home-about-left">
           <div>
             <h2>OUR STORY</h2>
             <p className="about-main-text">
@@ -36,16 +63,14 @@ function HomeAbout() {
               Alto Sax mouthpiece, musicians have loved our work.
             </p>
             <p className="about-supplemental-text">
-              Join the ranks
-               of saxophonists and woodwind players who trust
+              Join the ranks of saxophonists and woodwind players who trust
               Beechler for their mouthpiece and accessory needs. Elevate your
               playing experience today.
             </p>
           </div>
-        </div>
+        </Col>
 
-        <div className="col-md-8 home-about-right">
-          <div className="carousel-wrapper">
+        <Col md={8} lg={9} className="home-about-right">
             <Carousel
               showThumbs={false}
               showStatus={false}
@@ -59,25 +84,25 @@ function HomeAbout() {
             >
               <div className="slide">
                 <img src={about1} alt="your alt text" />
-                <p className="about-section-tag">Unleash your sound.</p>
+                <p className="our-story-tag">Unleash your sound.</p>
               </div>
               <div className="slide">
                 <img src={about2} alt="your alt text" />
-                <p className="about-section-tag">Crafted to perfection.</p>
+                <p className="our-story-tag">Crafted to perfection.</p>
               </div>
               <div className="slide">
                 <img src={about3} alt="your alt text" />
-                <p className="about-section-tag">Experience the difference.</p>
+                <p className="our-story-tag">Experience the difference.</p>
               </div>
               <div className="slide">
                 <img src={about4} alt="your alt text" />
-                <p className="about-section-tag">A firmament of industry.</p>
+                <p className="our-story-tag">A firmament of industry.</p>
               </div>
             </Carousel>
-          </div>
-        </div>
-      </div>
-    </div>
+
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
