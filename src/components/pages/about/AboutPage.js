cc274b6212ac56e05aba8dfaaef0972e21d78a27
Vikram Singh <v.p.singh@gmail.com>
2023-06-23 01:53:32 -0700

    Updated favicon and about and faq pages.

--- src/components/pages/about/AboutPage.js
+++ src/components/pages/about/AboutPage.js
@@ -4,22 +4,44 @@ import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import Masonry from "react-masonry-css";
 import { FiMusic } from "react-icons/fi";
 import arbLoop from "../../../assets/img/about/arb-loop.mp4";
+import jazzlight from "../../../assets/img/about/jazzlight.mp4";
 import NAMM2019 from "../../../assets/img/about/NAMM-2019.jpg";
 import Group from "../../../assets/img/about/Group.jpg";
 import Jim from "../../../assets/img/about/Jim.jpg";
+import Jim2 from "../../../assets/img/about/Jim2.png";
+import Charles from "../../../assets/img/about/Charles.jpg";
+import fiftiesAd from "../../../assets/img/about/1953ad.JPG";
+import history from "../../../assets/img/about/history.jpg";
+import elmer from "../../../assets/img/about/elmer.jpg";
+import kenny from "../../../assets/img/about/kenny.jpg";
+import namm20 from "../../../assets/img/about/namm20.jpg";
+import Max from "../../../assets/img/about/Max.jpg";
+import isaac from "../../../assets/img/about/Isaac.jpg";
+import kid from "../../../assets/img/about/kid.jpg";
+import everette from "../../../assets/img/about/everette.jpg";
+
 import { FaHeart } from "react-icons/fa";
+import beechlerArb from "../../../assets/img/home/beechler-arb.png";
+/*import colored american flag icon*/
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFlagUsa } from "@fortawesome/free-solid-svg-icons";
+
+/*import family icon*/
+
+import { FaAmericanSignLanguageInterpreting } from "react-icons/fa";
+import { FaFamily } from "react-icons/fa";
 
 const timelineData = [
   {
     type: "text",
     content:
       "Founded by Elmer Beechler in 1942, we design, manufacture, and distribute outstanding saxophone and clarinet mouthpieces and woodwind accessories.",
-    image: NAMM2019,
+    image: elmer,
   },
   {
     type: "text",
     content:
-      "Remlé Musical Products, Inc. has been under the direction of Elmer Beechler’s daughter, Judy Beechler Roan, since 1993.",
+      "Remlé Musical Products, Inc. has been under the direction of Elmer Beechler’s daughter, Judy Roan Beechler, since 1993.",
     image: Group,
   },
   {
@@ -49,15 +71,19 @@ const masonryImages = [
   NAMM2019,
   Group,
   Jim,
-  NAMM2019,
+  history,
+  Jim2,
+  fiftiesAd,
+  kenny,
+  namm20,
+  Charles,
+  Max,
+  isaac,
+  elmer,
+  kid,
+  everette,
   Group,
-  Jim,
-  NAMM2019,
-  Group,
-  Jim,
-  NAMM2019,
-  Group,
-  Jim,
+
   // Add more images here
 ];
 
@@ -67,18 +93,46 @@ const AboutPage = () => {
       <div className="about-page">
         <div className="about-header">
           <h1>Remlé Musical Products</h1>
-          <div className="about-video">
-            <video src={arbLoop} autoPlay loop muted className="about-video"/>
-          </div>
+
           <div className="about-text">
             <p>
-              We are a family-owned <FaHeart className="about-heart" /> women-led business, <br/>
+              Family-owned women-led business, <br />
+              <div className="about-video">
+                <video
+                  src={arbLoop}
+                  autoPlay
+                  loop
+                  muted
+                  className="about-video"
+                />
+              </div>
               making mouthpieces for over 75 years.
             </p>
           </div>
         </div>
 
         <div className="parallax-container">{renderParallaxSections()}</div>
+
+        <div className="about-header">
+          <h1>American Made Since 1942</h1>
+          <div className="about-video2">
+            <video
+              src={jazzlight}
+              autoPlay
+              loop
+              muted
+              className="about-video2"
+            />
+          </div>
+          <div className="about-text">
+            <p>
+              Discover the joy of playing at your full potential with Remlé
+              Musical Products.
+              <br />
+              <FaHeart className="about-heart" />
+            </p>
+          </div>
+        </div>
 
         <div className="masonry-container">
           <Masonry
--- src/components/pages/faq/FAQs.js
+++ src/components/pages/faq/FAQs.js
@@ -1,61 +1,61 @@
 import React, { useState } from "react";
-import { Container, Accordion, AccordionSummary, AccordionDetails, Typography } from "@mui/material";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { makeStyles } from '@mui/styles';
+import {
+  Container,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
   root: {
     fontFamily: "'Josefin Sans', sans-serif",
-    backgroundColor: '#fdfdfe',
-    color: '#2a3132',
+    backgroundColor: "#fdfdfe",
+    color: "#2a3132",
   },
   title: {
-    fontSize: '3rem',
-    fontWeight: '700',
-    marginTop: '1rem',
-    marginBottom: '1rem',
+    fontSize: "3rem",
+    fontWeight: "700",
+    marginTop: "1rem",
+    marginBottom: "1rem",
   },
   heading: {
-    borderRadius: '.75rem',
-    fontSize: '2rem',
-    fontWeight: '600',
-    color: '#0c0b0b',
-    marginTop:'1rem',
-    border:'none',
-    
+    borderRadius: ".75rem",
+    fontSize: "2rem",
+    fontWeight: "600",
+    color: "#0c0b0b",
+    marginTop: "1rem",
+    border: "none",
   },
   answer: {
-    color: '#0c0b0b',
-    fontSize: '1.5rem',
-    fontWeight: '400',
-    marginTop: '1rem',
-    marginBottom: '0',
-    border:'none',
-
+    color: "#0c0b0b",
+    fontSize: "1.5rem",
+    fontWeight: "400",
+    marginTop: "1rem",
+    marginBottom: "0",
+    border: "none",
   },
   accordion: {
-    borderRadius: '.75rem',
-    marginBottom: '2rem',
-    '&:last-child': {
-      marginBottom: '1rem',
+
+    marginBottom: "2rem",
+    "&:last-child": {
+      marginBottom: "1rem",
     },
-    '&::before': {
-      backgroundColor: 'transparent',
+    "&::before": {
+      backgroundColor: "transparent",
     },
   },
   accordionSummary: {
-    borderRadius: '.75rem',
-    backgroundColor: '#fbb034',
-    backgroundImage: 'linear-gradient(15deg, #fbb034 0%, #ffdd00 74%)',
-    color: '#fdfdfe',
+    backgroundColor: "#fbb034",
+    color: "#fdfdfe",
   },
   accordionDetails: {
-    backgroundColor: '#fdfdfe',
+    backgroundColor: "#fdfdfe",
+    backgroundImage: "linear-gradient(15deg, #fbb020 15%, #ffdd00 50%)",
+
   },
 });
-
 
 function FAQs() {
   const [expanded, setExpanded] = useState(false);
@@ -68,54 +68,56 @@ function FAQs() {
   const faqs = [
     {
       question: "What materials are used in Beechler mouthpieces?",
-      answer: "All Beechler and ARB mouthpieces have been lab tested and proven safe for all long and short term human contact according to Reach compliance standards. Bellite & Metal mouthpieces are cast by the Lost Wax method in surgical stainless steel."
+      answer:
+        "All Beechler and ARB mouthpieces have been lab tested and proven safe for all long and short term human contact according to Reach compliance standards. Bellite & Metal mouthpieces are cast by the Lost Wax method in surgical stainless steel. All bite plates on metal mouthpieces are inlaid using rubber and are not applied with epoxy.",
     },
     {
-      question: "What are the tip openings for saxophone and clarinet mouthpieces?",
-      answer: "Saxophone tip openings range from 4-10. Clarinet tip openings range from 1-12."
+      question:
+        "What are the tip openings for saxophone and clarinet mouthpieces?",
+      answer:
+        "Saxophone tip openings range from 4-10. Clarinet tip openings range from 1-12.",
     },
     {
       question: "Can I use Legere reeds with Beechler mouthpieces?",
-      answer: "Yes, you can use Legere reeds with our mouthpieces. However, this requires a different facing length. Please specify if you are using Legere reeds when ordering."
+      answer:
+        "Yes, you can use Legere reeds with our mouthpieces. However, this requires a different facing length. Please specify if you are using Legere reeds when ordering.",
     },
     {
       question: "What is the warranty on Beechler mouthpieces and accessories?",
-      answer: "Our mouthpieces, accessories, and services come with a 1-year warranty." 
+      answer:
+        "Our mouthpieces, accessories, and services come with a 1-year warranty.",
     },
     {
       question: "Can I get a custom mouthpiece made?",
-      answer: "Yes, we offer the service of creating a custom mouthpiece just for you. Please contact us for more details."
-    }
+      answer:
+        "Yes! We offer the service of creating a custom mouthpiece just for you. Please contact us for more details.",
+    },
   ];
 
   return (
     <Container className={classes.root}>
-<h1 className={classes.title}>
-        Frequently Asked Questions
-      </h1>
+      <h1 className={classes.title}>Frequently Asked Questions</h1>
 
       {faqs.map((faq, index) => (
-  <Accordion 
-    key={index} 
-    className={classes.accordion} 
-    expanded={expanded === `panel${index}`} 
-    onChange={handleChange(`panel${index}`)}
-  >
-    <AccordionSummary className={classes.accordionSummary}
-      expandIcon={<ExpandMoreIcon />}
-      aria-controls={`panel${index}d-content`}
-      id={`panel${index}d-header`}
-    >
-      <h2 className={classes.heading}>{faq.question}</h2>
-    </AccordionSummary>
-    <AccordionDetails className={classes.accordionDetails}>
-      <h5 className={classes.answer}>
-        {faq.answer}
-      </h5>
-    </AccordionDetails>
-  </Accordion>
-))}
-
+        <Accordion
+          key={index}
+          className={classes.accordion}
+          expanded={expanded === `panel${index}`}
+          onChange={handleChange(`panel${index}`)}
+        >
+          <AccordionSummary
+            className={classes.accordionSummary}
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel${index}d-content`}
+            id={`panel${index}d-header`}
+          >
+            <h2 className={classes.heading}>{faq.question}</h2>
+          </AccordionSummary>
+          <AccordionDetails className={classes.accordionDetails}>
+            <h5 className={classes.answer}>{faq.answer}</h5>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </Container>
   );
 }
