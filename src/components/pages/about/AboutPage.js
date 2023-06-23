d3bebaebbbdc75585c3c0d1302a4182671827911
Vikram Singh <v.p.singh@gmail.com>
2023-06-22 17:11:42 -0700

    Continued about section buildout.

--- src/components/pages/about/AboutPage.js
+++ src/components/pages/about/AboutPage.js
@@ -1,75 +1,98 @@
 import "../../../styles/AboutPage.css";
-
-
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+import Masonry from "react-masonry-css";
 import { FiMusic } from "react-icons/fi";
-import { Container, Row, Col } from 'react-bootstrap';
-import Masonry from 'react-masonry-css';
 import arbLoop from "../../../assets/img/about/arb-loop.mp4";
 import NAMM2019 from "../../../assets/img/about/NAMM-2019.jpg";
 import Group from "../../../assets/img/about/Group.jpg";
 import Jim from "../../../assets/img/about/Jim.jpg";
-import './AboutPage.css'; // Assuming you have a separate CSS file
+import { FaHeart } from "react-icons/fa";
 
 const timelineData = [
   {
     type: "text",
-    content: "Founded by Elmer Beechler in 1942, we design, manufacture, and distribute outstanding saxophone and clarinet mouthpieces and woodwind accessories.",
+    content:
+      "Founded by Elmer Beechler in 1942, we design, manufacture, and distribute outstanding saxophone and clarinet mouthpieces and woodwind accessories.",
     image: NAMM2019,
   },
   {
     type: "text",
-    content: "Remlé Musical Products, Inc. has been under the direction of Elmer Beechler’s daughter, Judy Beechler Roan, since 1993.",
+    content:
+      "Remlé Musical Products, Inc. has been under the direction of Elmer Beechler’s daughter, Judy Beechler Roan, since 1993.",
     image: Group,
   },
   {
     type: "text",
-    content: "We believe we make the best mouthpieces in the world for the professional musician and players who want to play well for the simple pleasure and joy of it.",
+    content:
+      "We believe we make the best mouthpieces in the world for the professional musician and players who want to play well for the simple pleasure and joy of it.",
     image: Jim,
   },
 ];
 
+const renderParallaxSections = () => {
+  return timelineData.map((item, index) => (
+    <Parallax
+      key={index}
+      className={`parallax-section ${index % 2 === 0 ? "inverse" : ""}`}
+      y={[-30, 30]}
+    >
+      <img src={item.image} alt="" className="parallax-image" />
+      <div className="parallax-text">
+        <p>{item.content}</p>
+      </div>
+    </Parallax>
+  ));
+};
+
+const masonryImages = [
+  NAMM2019,
+  Group,
+  Jim,
+  NAMM2019,
+  Group,
+  Jim,
+  NAMM2019,
+  Group,
+  Jim,
+  NAMM2019,
+  Group,
+  Jim,
+  // Add more images here
+];
+
 const AboutPage = () => {
   return (
-    <div className="about-page">
-      <div className="about-header">
-        <h1>About Remle Musical Products</h1>
-        <div className="about-video">
-          <video src={arbLoop} autoPlay loop muted />
+    <ParallaxProvider>
+      <div className="about-page">
+        <div className="about-header">
+          <h1>Remlé Musical Products</h1>
+          <div className="about-video">
+            <video src={arbLoop} autoPlay loop muted className="about-video"/>
+          </div>
+          <div className="about-text">
+            <p>
+              We are a family-owned <FaHeart className="about-heart" /> women-led business, <br/>
+              making mouthpieces for over 75 years.
+            </p>
+          </div>
         </div>
-        <div className="about-text">
-          <p>
-            We are a women-owned, family-owned, handcrafted artisans business.
-          </p>
+
+        <div className="parallax-container">{renderParallaxSections()}</div>
+
+        <div className="masonry-container">
+          <Masonry
+            breakpointCols={{ default: 4, 1100: 3, 700: 2, 500: 1 }}
+            className="masonry-grid"
+            columnClassName="masonry-grid_column"
+          >
+            {masonryImages.map((image, index) => (
+              <img key={index} src={image} alt="" />
+            ))}
+          </Masonry>
         </div>
       </div>
-
-      <Container fluid>
-        {timelineData.map((item, index) => (
-          <Row className={`timeline-section ${index % 2 === 0 ? 'left' : 'right'}`}>
-            <Col md={6}>
-              <motion.div className="timeline-image" whileHover={{ scale: 1.1 }}>
-                <img src={item.image} alt="Timeline" />
-              </motion.div>
-            </Col>
-            <Col md={6}>
-              <motion.div className="timeline-content" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>
-                <p>{item.content}</p>
-              </motion.div>
-            </Col>
-          </Row>
-        ))}
-      </Container>
-
-      <Masonry
-        breakpointCols={{ default: 4, 1100: 3, 700: 2, 500: 1 }}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {/* Add your images here */}
-      </Masonry>
-    </div>
+    </ParallaxProvider>
   );
 };
 
