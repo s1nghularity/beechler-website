45b540d4c2ddce073c4ceec4283cd87a105e4b30
Vikram Singh <v.p.singh@gmail.com>
2023-07-14 02:18:13 -0700

    updated overall layout design of top carousel and our story carousel

--- src/components/pages/home/HomeAbout.js
+++ src/components/pages/home/HomeAbout.js
@@ -4,40 +4,19 @@ import { Container, Row, Col } from 'react-bootstrap';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import about1 from "../../../assets/img/home/about/about1.jpg";
 import about2 from "../../../assets/img/home/about/about2.jpg";
+import about2h from "../../../assets/img/home/about/hands2.png";
 import about3 from "../../../assets/img/home/about/about3.jpg";
-import about4 from "../../../assets/img/home/the-hair.png";
+import about4 from "../../../assets/img/home/about/nbc-elmer.png";
+import about4bw from "../../../assets/img/home/about/nbc.png";
+import about5 from "../../../assets/img/home/about/hands2.png";
+
 import "../../../styles/HomeAbout.css";
 
 function HomeAbout() {
 
-  useEffect(() => {
-    const handleWindowResize = () => {
-      const screenWidth = window.innerWidth;
-      const customColumnWidth = getCustomColumnWidth(screenWidth);
-      document.documentElement.style.setProperty(
-        "--custom-column-width",
-        customColumnWidth
-      );
-    };
 
-    window.addEventListener("resize", handleWindowResize);
-
-    // Cleanup the event listener
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
-
-  const getCustomColumnWidth = (screenWidth) => {
-    if (screenWidth >= 768 && screenWidth <= 1312) {
-      return "15%";
-    }
-
-    return null;
-  };
-  
 function handleSlideChange(index) {
-  const slides = document.querySelectorAll(".slide");
+  const slides = document.querySelectorAll(".our-story-slide");
   slides.forEach((slide, i) => {
     const caption = slide.querySelector(".our-story-tag");
     if (caption) {
@@ -59,9 +38,9 @@ function handleSlideChange(index) {
   return (
     <Container fluid className="home-about stripe-home-about">
       <Row className="home-about-row">
-        <Col md={4} lg={3} className="home-about-left">
+        <Col md={12} lg={12} className="home-about-left">
           <div>
-            <h2>OUR STORY</h2>
+            <h2 className="home-about-left-title">OUR STORY</h2>
             <p className="about-main-text">
             For over 77 years professional musicians and those who play for the joy and pleasure of the music have favored Beechler Mouthpieces.
             <br/><br/> 
@@ -70,7 +49,7 @@ function handleSlideChange(index) {
           </div>
         </Col>
 
-        <Col md={8} lg={9} className="home-about-right">
+        <Col md={12} lg={12} className="home-about-right">
             <Carousel
               showThumbs={false}
               showStatus={false}
@@ -81,22 +60,37 @@ function handleSlideChange(index) {
               transitionTime={2000}
               onChange={handleSlideChange}
               stopOnHover={false}
+              draggable={true}
+              swipeable={true}
+              className="our-story-carousel"
             >
-              <div className="slide">
+              <div className="our-story-slide">
                 <img src={about1} alt="your alt text" />
                 <p className="our-story-tag">Unleash your sound.</p>
               </div>
-              <div className="slide">
+              <div className="our-story-slide">
                 <img src={about2} alt="your alt text" />
                 <p className="our-story-tag">Crafted to perfection.</p>
               </div>
-              <div className="slide">
+              <div className="our-story-slide">
+                <img src={about2h} alt="your alt text" />
+                <p className="our-story-tag">Crafted to perfection.</p>
+              </div>
+              <div className="our-story-slide">
                 <img src={about3} alt="your alt text" />
                 <p className="our-story-tag">Experience the difference.</p>
               </div>
-              <div className="slide">
+              <div className="our-story-slide-nbc">
                 <img src={about4} alt="your alt text" />
-                <p className="our-story-tag">A firmament of industry.</p>
+                <p className="our-story-tag">Unmatched in the industry.</p>
+              </div>
+              <div className="our-story-slide">
+                <img src={about4bw} alt="your alt text" />
+                <p className="our-story-tag">Timeless style and design.</p>
+              </div>
+              <div className="our-story-slide">
+                <img src={about5} alt="your alt text" />
+                <p className="our-story-tag">Pioneers of technique.</p>
               </div>
             </Carousel>
 
