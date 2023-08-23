c8f6bd36b7b4960c7e44fdf41c96d8570fcb0f8d
Vikram Singh <v.p.singh@gmail.com>
2023-08-23 16:44:08 -0700

    Fixed navbar mobile ux/ui and two word spacing (tip openings) navlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,70 +50,72 @@ function NavBar() {
   const links = [
     { path: "/", label: "Home" },
     { path: "/products", label: "Products" },
-    { path: "/tip-openings", label: "Tip Openings" },
+    { path: "/tip-openings", label: "Tip Openings", className: "spacing" },
     { path: "/dealers", label: "Dealers" },
     { path: "/artists", label: "Artists" },
     { path: "/gallery", label: "Gallery" },
     { path: "/about", label: "About" },
-
   ];
 
   return (
-
-      <Navbar expanded={expanded} expand="md" className="navbar">
-        <div className="navbar-header">
-          {!isDesktop && (
-            <Navbar.Toggle
-              aria-controls="basic-navbar-nav"
-              onClick={handleToggleClick}
-              className={`hamburger ${togglerToggled ? "toggled" : ""}`}
-            >
-              <span
-                className={`icon-bar top-bar ${
-                  togglerToggled ? "toggled" : ""
-                }`}
-              ></span>
-              <span
-                className={`icon-bar middle-bar ${
-                  togglerToggled ? "toggled" : ""
-                }`}
-              ></span>
-              <span
-                className={`icon-bar bottom-bar ${
-                  togglerToggled ? "toggled" : ""
-                }`}
-              ></span>
-            </Navbar.Toggle>
-          )}
-          <NavbarBrand href="/" className="navbar-brand">
-            <img src={logo} alt="Logo" width="100" height="50" />
-          </NavbarBrand>
-        </div>
-
-        <Nav className="nav-link-wrapper" style={collapseStyles}>
-          {links.map(({ path, label }) => (
-            <Nav.Link
-              key={path}
-              as={Link}
-              to={path}
-              className={
-                location.pathname === path ? "nav-link active-link" : ""
-              }
-            >
-              {label}
-            </Nav.Link>
-          ))}
-
-          <HashLink
-            to="/#contact"
-            onClick={handleContactClick}
-            className="contact-button"
+<Navbar expanded={expanded} expand="md" className={`navbar ${expanded ? "expanded" : ""}`}>
+      <div className="navbar-header">
+        {!isDesktop && (
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={handleToggleClick}
+            className={`hamburger ${togglerToggled ? "toggled" : ""}`}
           >
-            <FontAwesomeIcon icon={faEnvelope} />
-          </HashLink>
-          
-        </Nav>
-      </Navbar>
+            <span
+              className={`icon-bar top-bar ${togglerToggled ? "toggled" : ""}`}
+            ></span>
+            <span
+              className={`icon-bar middle-bar ${
+                togglerToggled ? "toggled" : ""
+              }`}
+            ></span>
+            <span
+              className={`icon-bar bottom-bar ${
+                togglerToggled ? "toggled" : ""
+              }`}
+            ></span>
+          </Navbar.Toggle>
+        )}
+        <NavbarBrand href="/" className="navbar-brand">
+          <img src={logo} alt="Logo" width="100" height="50" />
+        </NavbarBrand>
+      </div>
+
+      <Nav className="nav-link-wrapper" style={collapseStyles}>
+      {links.map(({ path, label, className }) => (
+  <Nav.Link
+    key={path}
+    as={Link}
+    to={path}
+    className={`nav-link ${location.pathname === path ? "active-link" : ""} ${className || ""}`}
+  >
+    {label === "Tip Openings" ? (
+      <div className="navbar-tip-openings">
+        <span>Tip</span>
+        <span>Openings</span>
+      </div>
+    ) : (
+      label
+    )}
+  </Nav.Link>
+))}
+
+
+
+        <HashLink
+          to="/#contact"
+          onClick={handleContactClick}
+          className="contact-button"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </HashLink>
+      </Nav>
+    </Navbar>
   );
 }
 
