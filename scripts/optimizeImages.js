4b54255c6c138622ff6b63258d9fd2bf768682c2
Vikram Singh <v.p.singh@gmail.com>
2023-08-29 21:40:45 -0700

    full site image optimization update

---
+++ scripts/optimizeImages.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const tinify = require('tinify');
+
+// Your TinyPNG API key
+tinify.key = "2rpCH3B4HTYT9nMCYm5WcSFf8HpTwlbx";
+
+// Function to optimize image
+const optimizeImage = async (srcPath, destPath) => {
+  try {
+    const source = await tinify.fromFile(srcPath);
+    await source.toFile(destPath);
+    console.log(`Optimized: ${destPath}`);
+  } catch (err) {
+    console.error(`Error optimizing ${srcPath}: ${err.message}`);
+  }
+};
+
+// Function to walk through directories and optimize images
+const walkDir = async (srcDir, destDir) => {
+  const files = fs.readdirSync(srcDir);
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir);
+  }
+  for (const file of files) {
+    const srcPath = path.join(srcDir, file);
+    const destPath = path.join(destDir, file);
+    const stat = fs.statSync(srcPath);
+    if (stat.isDirectory()) {
+      await walkDir(srcPath, destPath);
+    } else if (['.png', '.jpg', '.jpeg'].includes(path.extname(srcPath).toLowerCase())) {
+      await optimizeImage(srcPath, destPath);
+    }
+  }
+};
+
+// Start the optimization
+(async () => {
+  const srcRootDir = '../src/assets/img';  // Replace with your source directory
+  const destRootDir = '../src/assets/img2'; // Replace with your destination directory
+  await walkDir(srcRootDir, destRootDir);
+})();
