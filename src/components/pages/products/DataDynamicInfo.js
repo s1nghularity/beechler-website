ab2e6bae389d6c82c51bbcbbbf7bfbcff9a3ea9f
Vikram Singh <v.p.singh@gmail.com>
2023-09-14 18:02:15 -0700

    Update DataDynamicInfo.js

--- src/components/pages/products/DataDynamicInfo.js
+++ src/components/pages/products/DataDynamicInfo.js
@@ -1,42 +1,50 @@
 export const productInfo = {
   "Beechler Diamond": {
+    category: "Beechler Diamond",
     description:
       "Beechler Diamond Inlay Alto mouthpieces come in three bore sizes. The Clarinet, Tenor, and Baritone are designed with a medium open chamber and medium baffle. The composite material used for these mouthpieces is a sturdy combination of the qualities of traditional hard rubber and new-age plastics designed to utilize the full dynamic range of the instrument. Facing lengths available in Standard, Medium, and Long.",
     bores: ["Small", "Medium", "Large"],
   },
 
   "Beechler Tonalex": {
+    category: "Beechler Tonalex",
     description:
       "Beechler Tonalex Alto mouthpiece has medium baffles with open chambers. The Clarinet and Tenor mouthpieces have a medium open chamber. The white acoustic resin in these mouthpieces favors the softer singing qualities of the instrument. Facing lengths available in Standard, Medium, and Long.",
     bores: ["Small", "Medium", "Large"],
   },
 
   "Beechler Bellite": {
+    category: "Beechler Bellite",
     description:
       "Beechler Bellite Metal Mouthpiece is cast from the finest surgical stainless steel. This material is averse to corrosion, breakage, or change in playing characteristics. They are cast by the time-honored lost wax method, at a temperature of 2000 degrees, and will maintain consistent durability. The hardness of this metal provides an ultra-fast response time to bring out the brilliance of the overtone series. The Bellite alto and Tenor models feature a medium-high baffle and open chamber which supports a brilliant contemporary sound as well as a subdued ballad sound. The Bellite Soprano and Baritone have an open baffle. Ligatures and caps are provided with all Beechler Bellite mouthpieces.",
   },
 
   "Beechler Hard Rubber": {
+    category: "Beechler Hard Rubber",
     description:
       "Made from quality hard rubber. The hardness of the rubber and the size and shape of the chamber produce a dark, traditional sound.",
   },
 
   "ARB Metal": {
+    category: "ARB Metal",
     description:
       "ARB metal mouthpieces feature high baffles and step-off chambers for the Alto, Tenor, and Baritone. Like the Beechler Bellite, they are cast by the time-honored lost wax method, at a temperature of 2000 degrees, and will maintain consistent durability. They are extremely versatile and can be played with extraordinary brightness or with a soft and mellow sound. These mouthpieces are the culmination of years of development in arriving at the best playing combination of tone chambers, facings, and density of metal. They have excellent brilliance, response, and depth of tone. Ligatures and caps are provided with all ARB metal mouthpieces.",
   },
 
   "ARB Great Neck Originals": {
+    category: "ARB Great Neck Originals",
     description:
       "ARB Great Neck Original Mouthpieces are made from the blanks of the 1940s. These mouthpieces were given this name by Arnold Brilhart because of the location of his original factory in Great Neck, New York. The Alto and Tenor are large bore, medium baffle mouthpieces. The Clarinet has a medium open bore. The white pigment in these mouthpieces helps to produce the compact and subtle sound of those years. These are limited edition mouthpieces. They will never be available again after the current stock is gone.",
   },
 
   "Beechler Customized": {
+    category: "Beechler Customized",
     description:
       "The Musicians and artisans at Remlé Musical Products have worked with many outstanding professional musicians over the years. We have created mouthpieces for these gifted and supremely talented individuals and have learned from them in the process. This experience has helped us create a customized series of Beechler & ARB lines. The custom mouthpieces offer a specialized contour applied to the baffle of a small bore mouthpiece or a metal mouthpiece. This alternate approach to the creation of a mouthpiece provides specialized acoustic and dynamic features desired by professionals. It allows for the subtleties and nuances that can enhance musicianship and give the individual player the confidence to experiment, experience, and to explore.",
   },
 
   "Accessories & Services": {
+    category: "Accessories & Services",
     description:
       "All customizations are done by hand. We offer a variety of services to customize and provide the best playing experience as well as accessories to help you maintain your mouthpiece. Our mouthpieces are handcrafted with extraordinary attention to detail, allowing for an almost infinite choice of fine mouthpieces to suit varying embouchures, reed preferences, choice of materials, playing styles, and situations. And if you're looking for something truly unique, we can even create a custom mouthpiece just for you.",
     material: "Please specify Beechler or ARB for any of the L50 series ligatures.",
