fc2f9664c905f03d29d349471adc9d61f2b566d2
Vikram Singh <v.p.singh@gmail.com>
2023-09-15 05:09:18 -0700

    Update DealersMap.js
    removed itemList type headline

--- src/components/pages/dealers/DealersMap.js
+++ src/components/pages/dealers/DealersMap.js
@@ -15,32 +15,31 @@ const generateDealerJSONLD = (locations) => {
   return {
     "@context": "https://schema.org",
     "@type": "ItemList",
-    "itemListElement": locations.map((location, index) => ({
+    itemListElement: locations.map((location, index) => ({
       "@type": "ListItem",
-      "position": index + 1,
-      "item": {
+      position: index + 1,
+      item: {
         "@type": "Store",
-        "name": location.name,
-        "address": {
+        name: location.name,
+        address: {
           "@type": "PostalAddress",
-          "streetAddress": location.address,
-          "addressLocality": location.city,
-          "addressRegion": location.state,
-          "postalCode": location.zip,
-          "addressCountry": location.country
+          streetAddress: location.address,
+          addressLocality: location.city,
+          addressRegion: location.state,
+          postalCode: location.zip,
+          addressCountry: location.country,
         },
-        "telephone": location.phone,
-        "url": location.website,
-        "geo": {
+        telephone: location.phone,
+        url: location.website,
+        geo: {
           "@type": "GeoCoordinates",
-          "latitude": location.latitude,
-          "longitude": location.longitude
-        }
-      }
-    }))
+          latitude: location.latitude,
+          longitude: location.longitude,
+        },
+      },
+    })),
   };
 };
-
 
 const DealersMap = () => {
   const [search, setSearch] = useState("");
@@ -73,9 +72,6 @@ const DealersMap = () => {
     });
     setFilteredLocations(result);
   }, [search]);
-  
-
-
 
   const handleMouseEnter = () => {
     const map = mapRef.current;
@@ -105,7 +101,6 @@ const DealersMap = () => {
     }
   };
 
-
   const handleLocationClick = (location) => {
     const map = mapRef.current;
     const markerRef = markerRefs.current[location.name];
@@ -126,18 +121,15 @@ const DealersMap = () => {
     script.type = "application/ld+json";
     script.innerHTML = JSON.stringify(jsonld);
     document.head.appendChild(script);
-  
+
     return () => {
       document.head.removeChild(script);
     };
   }, [locationData]);
-  
 
   return (
-    <Container fluid itemScope itemType="https://schema.org/Map">
-      <h1 className="dealer-map-title" itemProp="headline">
-        Making Music All Over The World
-      </h1>
+    <Container fluid>
+      <h1 className="dealer-map-title">Making Music All Over The World</h1>
       <MapContainer
         center={[38, -95]}
         zoom={4}
