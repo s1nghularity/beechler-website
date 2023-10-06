c758fd44f3244f84fdc8a5f7dabf6fb2b4903d0d
Vikram Singh <v.p.singh@gmail.com>
2023-10-06 11:24:50 -0700

    SEO updates
    replaced 192 512pngs as well as updated 404 page crawlability <meta name="robots" content="noindex" />

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -4,43 +4,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart, faStore, faHome } from '@fortawesome/free-solid-svg-icons';
+import { Helmet } from "react-helmet-async";
 
 const NotFound = () => {
   return (
-    <>
-      <meta title="404 - Page Not Found" />
-      <div className="not-found">
-        
-        <h2>Oops!</h2>
-        
-        <h1>404</h1>
-        
-        <img src={jazzPlayer} className='jazz-gif' alt="Jazz player" />
-        
-        <p>Page Not Found</p>
+    <div className="not-found">
+      <Helmet>
+        <title>404 - Page Not Found</title>
+        <meta name="robots" content="noindex" />
+      </Helmet>
 
-        <div className="not-found-buttons">
-          <Link to="/">
-            <button>
-              <FontAwesomeIcon icon={faHome} />
-              Home
-            </button>
-          </Link>
-          <Link to="/products">
-            <button>
-              <FontAwesomeIcon icon={faShoppingCart} />
-              Products
-            </button>
-          </Link>
-          <Link to="/dealers">
-            <button>
-              <FontAwesomeIcon icon={faStore} />
-              Dealers
-            </button>
-          </Link>
-        </div>
+      <h2>Oops!</h2>
+      <h1>404</h1>
+      <img src={jazzPlayer} className='jazz-gif' alt="Jazz player" />
+      <p>Page Not Found</p>
+
+      <div className="not-found-buttons">
+        <Link to="/">
+          <button>
+            <FontAwesomeIcon icon={faHome} />
+            Home
+          </button>
+        </Link>
+        <Link to="/products">
+          <button>
+            <FontAwesomeIcon icon={faShoppingCart} />
+            Products
+          </button>
+        </Link>
+        <Link to="/dealers">
+          <button>
+            <FontAwesomeIcon icon={faStore} />
+            Dealers
+          </button>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
